593103f6fbc6736ca0d860b1987701bc3471f944
andreabarbata <andrea.barbata.ab@gmail.com>
2015-11-01 23:32:12 +0100

    upload compiti

---
+++ homework_01.js
@@ -0,0 +1,390 @@
+//ex 1
+//iterativo
+var myv1=[1,2,-6,4,5];
+
+function s1(v){
+	var som=0;
+	for (var i=0;i< v.length;i++){
+		if (v[i]>0){
+			som+=v[i];
+		}	
+		else return som;
+	}
+	return som;
+}
+
+var ris = s1(myv1);
+console.log(ris);
+
+//ricorsivo
+var myv1r=[1,2,6,-4,5];
+var index1r=0
+var somma1r=0
+function sum1(v,i,s){
+	if ((v[i]>0) || (i<v.lenght)){
+		s+=v[i];
+		i++;
+		s=sum1(v,i,s)
+	}
+	return s
+}
+
+var ris = sum1(myv1r,index1r,somma1r);
+console.log(ris);
+ 
+//ex 2
+//iterativo
+var n2=3;
+
+function s2(num){
+	var som=0;
+	var num2=num
+	for (var i=0;i<=num2*2;i++){	
+		num+=1
+		if (((num)>=0) && (num)%2!=0);{
+			som+=(num);
+		}
+		
+	}
+	return som;
+}
+
+var ris=s2(n2);
+console.log(ris);
+
+//ricorsivo
+var num2=4;
+var somma2=0;
+var index2=1;
+function sumd2(n,s,i){
+	if ((i<=n)) {
+		s+=i;
+		i+=2;
+		s=sumd2(n,s,i)
+	}
+	return s
+}
+
+var ris = sumd2(num2,somma2,index2);
+console.log(ris);
+ 
+	
+//ex 3
+//iterativo
+var myv3=[1,5,9,-6,-7,2,3,4,7,1];
+
+function s3(v){
+	var som=0;
+	for (var i=0;i<v.length;i++){	
+		som+=v[i];
+		
+	}
+	
+	return som/v.length;
+}
+
+var ris=s3(myv3);
+console.log(ris);
+
+//ricorsivo
+var myv3r=[1,2,3,4,5,6,7,8,9,0];
+var index3r=0
+var somma3r=0
+function med3(v,i,s){
+	if ((i<v.length)) {
+		s+=v[i];
+		i++;
+		s=med3(v,i,s)
+	}
+	return (s)
+}
+
+var ris = med3(myv3r,index3r,somma3r)/myv.length;
+console.log(ris);
+
+//ex 4
+//iterativo
+var a4=6;
+var b4=4;
+function s4(n1,n2){
+	var som=0;
+	if (n1<=n2){
+		for (var i=0;i<=(n2-n1);i++){	
+			som+=(n1+i);
+		}
+	}
+	else {
+		for (var i=0;i<=(n1-n2);i++){	
+			som+=(n2+i);
+			}
+	}
+		
+	
+	return som
+}
+
+var ris=s4(a4,b4);
+console.log(ris);
+
+//ricorsivo
+var a4r=5;
+var b4r=7;
+var somma4r=0;
+var index4r=0;
+function f4(n1,n2,s,i){
+	if ((n1+i)<=n2) {
+		s+=(n1+i);
+		i++;
+		s=f4(n1,n2,s,i)
+	}
+	return (s)
+}
+
+var ris = f4(a4r,b4r,somma4r,index4r);
+console.log(ris);
+
+//ex 5
+//iterativo
+var a5=5;
+var b5=4;
+function s5(n1,n2){
+	var som=0;
+		for (var i=0;i<=(n2-1);i++){	
+			som+=(n1);
+		}
+	
+		
+	
+	return som
+}
+
+var ris=s5(a5,b5);
+console.log(ris);
+
+//ricorsivo
+var a5r=10;
+var b5r=5;
+var somma5r=0;
+var inde5r=0;
+function f5(n1,n2,s,i){
+	if ((n1-i)>0) {
+		s+=(n2);
+		i++;
+		s=f5(n1,n2,s,i)
+	}
+	return (s)
+}
+
+var ris = f5(a5r,b5r,somma5r,index5r);
+console.log(ris);
+
+//ex 6
+//iterativo
+var a6=8;
+var b6=4;
+function s6(n1,n2){
+	var q=0;
+	var resto=0;
+		for (var i=n2;i<=n1;i+=n2){	
+			q+=1;
+		}
+	var ans = q + " con resto di " + (n1-(n2*q));
+	return ans;
+
+}	
+	
+var ris=s6(a6,b6);
+console.log(ris);
+
+//ex 7
+//iterativo
+var a=2;
+var b=4;
+
+function mult7(n1,n2){
+	var som=0;
+		for (var i=0;i<=(n2-1);i++){	
+			som+=(n1);
+		}
+	
+		
+	
+	return som
+}
+
+function s7(n1,n2){
+	var pot=1;
+		for (var i=0;i<=(n2-1);i++){	
+			pot=mult7(pot,a)
+		}
+	
+	return pot;
+
+}	
+	
+var ris=s7(a7,b7);
+console.log(ris);
+
+//ex 8
+//iterativo
+var myv8=[2,6,9,1,7,2,3,0,4];
+var sq8=(Math.sqrt(myv.length)-1)
+var mx8=[]
+
+
+function s8(v){
+		var k=0;
+		for (var i=0; i<=sq;i++){
+			mx[i] = new Array() 
+			for (var j=0; j<=sq;j++){
+				mx[i][j]=v[k];
+				k++;
+			}
+			
+		}
+		return mx
+}	
+
+var ris=s8(myv8);
+console.log(ris)
+//ex 9
+//iterativo
+var myv9=[2,6,9,1,7,2,3,0];
+
+function s9(v1){
+		var k=0;
+		var n=(myv.length)-1
+		var v2=[]
+		for (var i=n; i>=0;i=i - 1){
+				v2[k]=v1[i]
+				k++
+		}
+		return v2
+}	
+
+var ris=s9(myv9);
+console.log(ris)
+
+//ex 10
+//iterativo
+var a10=3
+var n10=4
+
+
+
+function s10(n1,n2){
+		var v=[]
+		for (var i=0; i<=n2-1;i++){
+			v[i]=n1		
+			
+		}
+		return v
+		
+}	
+
+var ris=s10(a10,n10);
+console.log(ris);
+
+//ex 11
+ //iterativo
+ var myv11=[2,5,1,8,3,2,1];
+
+function s11(v1){
+		var p=[]
+		var d=[]
+		var np=0
+		var nd=0
+		var k=0
+		for (var i=0; i<=v1.length-1;i++){
+			 if (v1[i]%2!=0){
+				d[nd]=v1[i]
+				nd++
+				}
+			else {
+				p[np]=v1[i]
+				np++
+				}
+			 }
+
+		for (i=0;i<=d.length-1;i++){
+			v1[i]=d[i]
+		}
+		for (i=d.length;i<=v1.length-1;i++,k++){
+			v1[i]=p[k]
+		}
+		
+		return v1
+}
+
+var ris=s11(myv11);
+console.log(ris);
+
+
+function ex_1_I(array){
+	return s1(myv)
+}
+
+function ex_1_R(array){
+	return sum1(myv1r,index1r,somma1r)
+}
+
+function ex_2_I(num){
+	return s2(n2)
+}
+
+function ex_2_R(num){
+	return sumd2(num2,somma2,index2)
+}
+
+function ex_3_I(array){
+	return s3(myv3)
+}
+
+function ex_3_R(array){
+	return med3(myv3r,index3r,somma3r)
+}
+
+function ex_4_I(n1,n2){
+	return s4(a4,b4)
+}
+
+function ex_4_R(n1,n2){
+	return f4(a4r,b4r,somma4r,index4r)
+}
+
+function ex_5_I(n1,n2){
+	return s5(a5,b5)
+}
+
+function ex_5_R(n1,n2){
+	return f5(a5r,b5r,somma5r,index5r)
+}
+
+function ex_6_I(n1,n2){
+	return s6(a6,b6)
+}
+
+function ex_7_I(n1,n2){
+	return s7(a7,a8)
+}
+
+function ex_8_I(array){
+	return s8(myv8)
+}
+
+function ex_9_I(n1,n2){
+	return s9(a9,b9)
+}
+
+function ex_10_I(array){
+	return s10(myv10)
+}
+
+function ex_11_I(array){
+	return s11(myv11)
+}
+
+
+		
+
+
